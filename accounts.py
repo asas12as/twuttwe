109ff203efa5d66b0fe2ccbf9b5a4b3819f2e1ac
asas12as <70827470+asas12as@users.noreply.github.com>
2023-04-20 13:36:53 +0200

    Update accounts.py

--- accounts.py
+++ accounts.py
@@ -1,150 +1,160 @@
+# python accounts.py -i ../../data/twitter-creator.json -d regular -f 1
+# python accounts.py -i ../../data/twitter-creator.json -d proxy -f 1
 import sys
+import time
 import getopt
 import json
-import tweepy
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from SeleniumHelper import SeleniumHelper
+
+service = Service('D:\TAC\twitter-accounts-creator-bot-master\twitter-accounts-creator-bot-master\geckodriver-v0.33.0-win64\geckodriver.exe')
+driver = webdriver.Firefox(service=service)
+driver.implicitly_wait(10)
+driver.get('https://mobile.twitter.com/signup?type=email')
+class TwitterCreator(SeleniumHelper):
+
+	MOBILE_URL_CREATE = 'https://mobile.twitter.com/signup?type=email'
+	MOBILE_FIELD_SIGN_UP_NAME = '#oauth_signup_client_fullname'
+	MOBILE_FIELD_SIGN_UP_EMAIL = '#oauth_signup_client_phone_number'
+	MOBILE_FIELD_SIGN_UP_PASSWORD = '#password'
+	MOBILE_FIELD_SIGN_UP_USERNAME = '#custom_name'
+	MOBILE_BUTTON_SKIP_PHONE = '.signup-skip input'
+	MOBILE_BUTTON_INTERESTS = 'input[data-testid="Button"]'
+
+	DESKTOP_URL_CREATE = 'https://twitter.com/signup'
+	DESKTOP_URL_SKIP = 'https://twitter.com/account/add_username'
+	DESKTOP_URL_MAIN = 'https://twitter.com'
+	DESKTOP_FIELD_SIGN_UP_NAME = '#full-name'
+	DESKTOP_FIELD_SIGN_UP_EMAIL = '#email'
+	DESKTOP_FIELD_SIGN_UP_PASSWORD = '#password'
+	DESKTOP_FIELD_SIGN_UP_USERNAME = '#username'
+	DESKTOP_FIELD_SIGN_UP_PHONE = '#phone_number'
+	DESKTOP_FIELD_SIGN_UP_CODE = '#code'
+	DESKTOP_FIELD_SIGN_UP_SUGGESTION = '.suggestions > ul:nth-child(2) > li:nth-child(1) > button:nth-child(1)'	
+	DESKTOP_FIELD_LOGOUT = '#signout-form'
+	DESKTOP_BUTTON_SKIP_PHONE = '.signup-skip input'
+	DESKTOP_BUTTON_CALL_ME = 'input[name="call_me"]'
+	DESKTOP_BUTTON_INTERESTS = 'input[data-testid="Button"]'
+
+	DESKTOP_PAGE_CONTAINER = '#page-container'
+	DESKTOP_PAGE_PHONE = '.PageContainer'
+	DESKTOP_PAGE_INI = '#doc'
 
 
-class TwitterCreator:
+	def mobileCreateUser(self, row):
+		self.loadPage(self.DESKTOP_URL_CREATE)
+		self.waitAndWrite(self.DESKTOP_FIELD_SIGN_UP_NAME, row['name'])
+		self.submitForm(self.selectAndWrite(self.DESKTOP_FIELD_SIGN_UP_EMAIL, row['email']))
+		self.submitForm(self.waitAndWrite(self.DESKTOP_FIELD_SIGN_UP_PASSWORD, row['password']))
+		self.clickSelector(self.DESKTOP_BUTTON_SKIP_PHONE)
+		self.submitForm(self.waitAndWrite(self.DESKTOP_FIELD_SIGN_UP_USERNAME, row['username']))
+		self.waitAndClick(self.DESKTOP_BUTTON_INTERESTS)
+		#main_content > div.footer > form > input
+		time.sleep(9999)
+		# self.submitForm()
 
-    def start(self, callbacks, inputFile, fromRow, toRow, driverType):
-        try:
-            rows = json.loads(open(inputFile).read())
-            numElements = len(rows)
-        except:
-            numElements = 0
-        if numElements > 0:
-            if toRow == -1:
-                toRow = numElements
-            else:
-                if toRow > numElements:
-                    toRow = numElements
-            fromRow -= 1
-            if fromRow < numElements:
-                self.driver = self.getWebdriver(driverType)
-                for numRow in range(fromRow, toRow):
-                    row = rows[numRow]
-                    print('Processing row: ' + str(numRow))
-                    for callback in callbacks:
-                        callback(row)
-                    print('Processed.')
-                self.close()
-            else:
-                print('Index out of bounds')
-        else:
-            print('Data could not be extracted')
+	def desktopCreateUser(self, row):
+		self.loadPage(self.DESKTOP_URL_CREATE)
+		self.waitAndWrite(self.DESKTOP_FIELD_SIGN_UP_NAME, row['name'])
+		self.selectAndWrite(self.DESKTOP_FIELD_SIGN_UP_EMAIL, row['email'])
+		self.submitForm(self.selectAndWrite(self.DESKTOP_FIELD_SIGN_UP_PASSWORD, row['password']))
+		self.waitShowElement(self.DESKTOP_PAGE_CONTAINER)
+		self.loadPage(self.DESKTOP_URL_SKIP)
+		self.submitForm(self.waitAndWrite(self.DESKTOP_FIELD_SIGN_UP_USERNAME, row['username']))
+		self.waitShowElement(self.DESKTOP_PAGE_CONTAINER)
+		self.loadPage(self.DESKTOP_URL_MAIN)
+		time.sleep(9999)
 
-    def getWebdriver(self, driverType):
-        if driverType == 'proxy':
-            profile = webdriver.FirefoxProfile()
-            profile.set_preference("network.proxy.type", 1)
-            profile.set_preference("network.proxy.socks", "127.0.0.1")
-            profile.set_preference("network.proxy.socks_port", 9150)
-            profile.set_preference("network.proxy.socks_remote_dns", True)
-            profile.set_preference("places.history.enabled", False)
-            profile.set_preference("privacy.clearOnShutdown.offlineApps", True)
-            profile.set_preference("privacy.clearOnShutdown.passwords", True)
-            profile.set_preference(
-                "privacy.clearOnShutdown.siteSettings", True)
-            profile.set_preference("privacy.sanitize.sanitizeOnShutdown", True)
-            profile.set_preference("signon.rememberSignons", False)
-            profile.set_preference("network.cookie.lifetimePolicy", 2)
-            profile.set_preference("network.dns.disablePrefetch", True)
-            profile.set_preference("network.http.sendRefererHeader", 0)
-            profile.set_preference("javascript.enabled", False)
-            profile.set_preference("permissions.default.image", 2)
-            return webdriver.Firefox(profile)
-        elif driverType == 'headless':
-            return webdriver.PhantomJS()
-        else:
-            return webdriver.Firefox()
+	def desktopCreateUserPhone(self, row):
+		self.loadPage(self.DESKTOP_URL_CREATE)
+		self.waitAndWrite(self.DESKTOP_FIELD_SIGN_UP_NAME, row['name'])
+		self.selectAndWrite(self.DESKTOP_FIELD_SIGN_UP_EMAIL, row['email'])
+		self.submitForm(self.selectAndWrite(self.DESKTOP_FIELD_SIGN_UP_PASSWORD, row['password']))
+		self.submitForm(self.waitAndWrite(self.DESKTOP_FIELD_SIGN_UP_PHONE, row['phone']))
+		row['code'] = input('Code: ')
+		self.submitForm(self.waitAndWrite(self.DESKTOP_FIELD_SIGN_UP_CODE, row['code']))
+		self.waitAndClick(self.DESKTOP_FIELD_SIGN_UP_SUGGESTION)
+		self.submitFormSelector(self.DESKTOP_FIELD_SIGN_UP_USERNAME)
+		self.waitShowElement(self.DESKTOP_PAGE_CONTAINER)
+		self.loadPage(self.DESKTOP_URL_MAIN)
+		self.submitForm(self.waitShowElement(self.DESKTOP_FIELD_LOGOUT))
+		self.waitShowElement(self.DESKTOP_PAGE_INI)
 
-    def desktopCreateUserPhone(self, row):
-        consumer_key = 'emubaipEDnjyLM1jhG2Kb17Ny'
-        consumer_secret = 'GeGrxYbHhqeOQxgfpA39nTbHAPOzRsEL2IgHwju4xrEXcngstC'
-        access_token = '1041594267964256257-OUt3QJZf0aUEHyp2HoTcoRqGuGJ8gF'
-        access_token_secret = 'ebHRgfrU4abL9x9xHqqk5AlhLxMBszondRIOeBECnvOqh'
+	def start(self, callbacks, inputFile, fromRow, toRow, driverType):
+		try:
+			rows = json.loads(open(inputFile).read())
+			numElements = len(rows)
+		except:
+			numElements = 0
+		if numElements > 0:
+			if toRow == -1:
+				toRow = numElements
+			else:
+				if toRow > numElements:
+					toRow = numElements
+			fromRow -= 1
+			if fromRow < numElements:
+				self.driver = self.getWebdriver(driverType)
+				for numRow in range(fromRow, toRow):
+					row = rows[numRow]
+					print('Processing row: ' + str(numRow))
+					for callback in callbacks:
+						callback(row)
+					print('Processed.')
+				self.close()
+			else:
+				print('Index out of bounds')
+		else:
+			print('Data could not be extracted')
 
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
-
-        api = tweepy.API(auth)
-
-        # Register a new user on desktop
-        self.driver.get('https://twitter.com/i/flow/signup')
-        self.driver.implicitly_wait(2)
-        self.driver.find_element_by_xpath(
-            "//span[text()='Use email instead']").click()
-        self.driver.find_element_by_name('email').send_keys(row['Email'])
-        self.driver.find_element_by_name('password').send_keys(row['Password'])
-        self.driver.find_element_by_name('name').send_keys(row['FullName'])
-        self.driver.find_element_by_xpath("//span[text()='Next']").click()
-        self.driver.implicitly_wait(2)
-        self.driver.find_element_by_name(
-            'phone_number').send_keys(row['PhoneNumber'])
-        self.driver.find_element_by_xpath("//span[text()='Next']").click()
-        self.driver.implicitly_wait(2)
-        self.driver.find_element_by_xpath("//span[text()='Next']").click()
-        self.driver.implicitly_wait(2)
-        self.driver.find_element_by_xpath("//span[text()='Sign up']").click()
-        self.driver.implicitly_wait(5)
-
-        # Verify the email address
-        verification_link = self.getVerificationLink(row['Email'])
-        if verification_link:
-            self.driver.get(verification_link)
-            self.driver.implicitly_wait(2)
-            self.driver.find_element_by_xpath("//span[text()='Next']").click()
-            self.driver.implicitly_wait(2)
-            self.driver.find_element_by_xpath(
-                "//span[text()='Let's go']").click()
-            self.driver.implicitly_wait(5)
-            print('Account created for ' + row['Email'])
-            api.update_status(
-                "Hello, world! This is my first tweet with Python and Tweepy!")
-        else:
-            print('Could not get verification link for ' + row['Email'])
-
-    def getVerificationLink(self, email):
-        # This function should retrieve the verification link from the email sent to the user's email address
-        # You can use a third-party library like BeautifulSoup or lxml to parse the email
-        # and retrieve the verification link from the email's HTML content
-        # However, this is beyond the scope of this code template and requires additional configuration and setup
-        # For testing purposes only, this function returns a dummy verification link
-        return 'https://twitter.com/i/flow/signup/complete?flow_data=%7B%22show_sms_verification_dialog%22%3Atrue%2C%22search_surface%22%3A%22signup_flow%22%2C%22email%22%3A%22' + email + '%22%2C%22flow_origin%22%3A%22web%22%2C%22prefill_phone_number%22%3Atrue%2C%22phone_number%22%3A%22%22%7D'
-
-    def close(self):
-        self.driver.quit()
-
+	def getWebdriver(self, driverType):
+		if driverType == 'proxy':
+			profile = webdriver.FirefoxProfile()
+			profile.set_preference( "network.proxy.type", 1 )
+			profile.set_preference( "network.proxy.socks", "127.0.0.1" )
+			profile.set_preference( "network.proxy.socks_port", 9150 )
+			profile.set_preference( "network.proxy.socks_remote_dns", True )
+			profile.set_preference( "places.history.enabled", False )
+			profile.set_preference( "privacy.clearOnShutdown.offlineApps", True )
+			profile.set_preference( "privacy.clearOnShutdown.passwords", True )
+			profile.set_preference( "privacy.clearOnShutdown.siteSettings", True )
+			profile.set_preference( "privacy.sanitize.sanitizeOnShutdown", True )
+			profile.set_preference( "signon.rememberSignons", False )
+			profile.set_preference( "network.cookie.lifetimePolicy", 2 )
+			profile.set_preference( "network.dns.disablePrefetch", True )
+			profile.set_preference( "network.http.sendRefererHeader", 0 )
+			profile.set_preference( "javascript.enabled", False )
+			profile.set_preference( "permissions.default.image", 2 )
+			return webdriver.Firefox(profile)
+		elif driverType == 'headless':
+			return webdriver.PhantomJS()
+		else:
+			return webdriver.Firefox()
 
 def main(argv):
-    fromRow = 1
-    toRow = -1
-    inputFile = 'D:\\TAC\\AA.csv'
-    driverType = 'proxy'
-    opts, args = getopt.getopt(argv, "f:t:i:d:")
-    if opts:
-        for o, a in opts:
-            if o in ("-f"):
-                fromRow = int(a)
-            elif o in ("-t"):
-                toRow = int(a)
-            elif o in ("-i"):
-                inputFile = a
-            elif o in ("-d"):
-                driverType = a
-    while not inputFile:
-        inputFile = input('Input file path: ')
-    creator = TwitterCreator()
-    print('Process started')
-    creator.start(callbacks=[creator.desktopCreateUserPhone],inputFile=inputFile, fromRow=fromRow, toRow=toRow, driverType=driverType)
-    # print(inputFile)
+	fromRow = 1
+	toRow = -1
+	inputFile = 'D:\TAC\AA.csv'
+	driverType = 'proxy'
+	opts, args = getopt.getopt(argv, "f:t:i:d:")
+	if opts:
+		for o, a in opts:
+			if o in ("-f"):
+				fromRow = int(a)
+			elif o in ("-t"):
+				toRow = int(a)
+			elif o in ("-i"):
+				inputFile = a
+			elif o in ("-d"):
+				driverType = a
+	while not inputFile:
+		inputFile = input('Input file path: ')
+	creator = TwitterCreator()
+	print('Process started')
+	creator.start(callbacks=[creator.desktopCreateUserPhone], inputFile=inputFile, fromRow=fromRow, toRow=toRow, driverType=driverType)
+	print(inputFile)
+	print('Process ended')
 
-    print('Process ended')
-
-
-with open('/TAC/AA.csv', 'r') as file:
-    contents = file.read()
-    print(contents)
 if __name__ == "__main__":
     main(sys.argv[1:])
